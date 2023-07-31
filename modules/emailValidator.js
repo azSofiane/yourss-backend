dc4efa0af7aa5e5a04675d5ef514ff940dd216b6
Sofiane <ahmedzaid.s@gmail.com>
2023-07-31 15:17:53 +0200

    les deux fonction dend email et validation email

---
+++ modules/emailValidator.js
@@ -0,0 +1,8 @@
+
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+function isValidEmail(email) {
+  return EMAIL_REGEX.test(email);
+}
+
+module.exports = { isValidEmail };
---
+++ modules/sendResetPasswordEmail.js
@@ -0,0 +1,35 @@
+const nodemailer = require('nodemailer');
+
+// Configuration de transport pour Nodemailer (utilisez votre propre service SMTP ou fournisseur d'e-mails)
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  auth: {
+    type: 'login',
+    user: 'yourslacapsule@gmail.com',
+    pass: 'yours!123',
+  },
+});
+
+function sendResetPasswordEmail(email, resetToken) {
+  // Créez le lien de réinitialisation avec le jeton et l'URL de réinitialisation de mot de passe (à remplacer par l'URL réelle de votre application)
+  const resetLink = `http://localhost:3000/reset-password?token=${resetToken}`;
+
+  // Configurations de l'e-mail
+  const mailOptions = {
+    from: 'yourslacapsule@gmail.com', //à changer dynamiquement
+    to: email,
+    subject: 'Réinitialisation de mot de passe',
+    html: `<p>Bonjour,</p><p>Vous avez demandé une réinitialisation de mot de passe.</p><p>Veuillez cliquer sur le lien ci-dessous pour réinitialiser votre mot de passe :</p><p><a href="${resetLink}">${resetLink}</a></p><p>Si vous n'avez pas effectué cette demande, veuillez ignorer cet e-mail.</p><p>Cordialement,<br>L'équipe de votre application</p>`,
+  };
+
+  // Envoi de l'e-mail
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Erreur lors de l\'envoi de l\'e-mail de réinitialisation de mot de passe:', error);
+    } else {
+      console.log('E-mail de réinitialisation de mot de passe envoyé avec succès:', info.response);
+    }
+  });
+}
+
+module.exports = { sendResetPasswordEmail };
