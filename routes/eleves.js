03bd59b3e91d72e6495283edb360aa26e5329054
Sofiane <ahmedzaid.s@gmail.com>
2023-08-04 15:50:46 +0200

    modifier email et mot de passe du professionnel et eleve ok cote backend

--- routes/eleves.js
+++ routes/eleves.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var router = express.Router()
 
+const { isValidEmail } = require('@modules/emailValidator');
+const {isStrongPassword} = require('@modules/passwordValidator');
+const bcrypt = require('bcrypt');
 const Eleve= require ('@models/eleves');
 const { cleanSpace } = require('@modules/cleanSpace')
 
@@ -33,6 +36,58 @@ router.post('/edit', async (req, res) => {
 
   // envoyer les modifications
   const updateResult = await Eleve.updateOne({ token }, champs);
+
+  if (updateResult.modifiedCount > 0) {
+    return res.json({ result: true, message: 'Mise à jour réussie !' });
+  } else {
+    return res.json({ result: false, message: 'Aucun changement effectuée' });
+  }
+});
+
+// Route pour modifier le email
+router.put('/editemail/:token', async (req, res) => {
+  const { email } = req.body;
+
+  // vérifier que le token existe dans la bdd
+  const isValidToken = await Eleve.findOne({ token: req.params.token });
+
+  if (!isValidToken) {
+    return res.json({ result: false, message: 'Token invalide. Accès non autorisé' });
+  }
+    // Vérifier si l'adresse e-mail est valide
+  if (!isValidEmail(email)) {
+    return res.json({ result: false, error: 'Adresse e-mail invalide' });
+  };
+
+  // envoyer les modifications
+  const updateResult = await Eleve.updateOne({ token: req.params.token }, {email: email});
+
+  if (updateResult.modifiedCount > 0) {
+    return res.json({ result: true, message: 'Mise à jour réussie !' });
+  } else {
+    return res.json({ result: false, message: 'Aucun changement effectuée' });
+  }
+});
+
+// Route pour modifier le mot de passe
+router.put('/editmotdepasse/:token', async (req, res) => {
+  const { mot_de_passe } = req.body;
+
+  // vérifier que le token existe dans la bdd
+  const isValidToken = await Eleve.findOne({ token: req.params.token });
+
+  if (!isValidToken) {
+    return res.json({ result: false, message: 'Token invalide. Accès non autorisé' });
+  }
+  // Vérifiez si le mot de passe est très fort
+  if (!isStrongPassword(mot_de_passe)) {
+    res.json({ result: false, error: 'Le mot de passe doit comporter au moins 8 caractères, dont au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial (@$!%*?&)' });
+    return;
+  };
+  const hash = bcrypt.hashSync(mot_de_passe, 10);
+
+  // envoyer les modifications
+  const updateResult = await Eleve.updateOne({ token: req.params.token }, {mot_de_passe: hash});
 
   if (updateResult.modifiedCount > 0) {
     return res.json({ result: true, message: 'Mise à jour réussie !' });
--- routes/professionnels.js
+++ routes/professionnels.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+const { isValidEmail } = require('@modules/emailValidator');
+const {isStrongPassword} = require('@modules/passwordValidator');
+const bcrypt = require('bcrypt');
 const Professionnel = require('@models/professionnels');
-
 const { cleanSpace } = require('@modules/cleanSpace');
 
 // todo - revoir verification pour mail, photo, mot de passe (token aussi) et date de naissance (voir si une nouvelle route ou plusieurs conditions dans la route de modification)
@@ -41,4 +42,55 @@ router.post('/edit', async (req, res) => {
   }
 });
 
+// Route pour modifier le email
+router.put('/editemail/:token', async (req, res) => {
+  const { email } = req.body;
+
+  // vérifier que le token existe dans la bdd
+  const isValidToken = await Professionnel.findOne({ token: req.params.token });
+
+  if (!isValidToken) {
+    return res.json({ result: false, message: 'Token invalide. Accès non autorisé' });
+  }
+    // Vérifier si l'adresse e-mail est valide
+  if (!isValidEmail(email)) {
+    return res.json({ result: false, error: 'Adresse e-mail invalide' });
+  };
+
+  // envoyer les modifications
+  const updateResult = await Professionnel.updateOne({ token: req.params.token }, {email: email});
+
+  if (updateResult.modifiedCount > 0) {
+    return res.json({ result: true, message: 'Mise à jour réussie !' });
+  } else {
+    return res.json({ result: false, message: 'Aucun changement effectuée' });
+  }
+});
+
+// Route pour modifier le mot de passe
+router.put('/editmotdepasse/:token', async (req, res) => {
+  const { mot_de_passe } = req.body;
+
+  // vérifier que le token existe dans la bdd
+  const isValidToken = await Professionnel.findOne({ token: req.params.token });
+
+  if (!isValidToken) {
+    return res.json({ result: false, message: 'Token invalide. Accès non autorisé' });
+  }
+  // Vérifiez si le mot de passe est très fort
+  if (!isStrongPassword(mot_de_passe)) {
+    res.json({ result: false, error: 'Le mot de passe doit comporter au moins 8 caractères, dont au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial (@$!%*?&)' });
+    return;
+  };
+  const hash = bcrypt.hash(mot_de_passe, 10);
+
+  // envoyer les modifications
+  const updateResult = await Professionnel.updateOne({ token: req.params.token }, {mot_de_passe: hash});
+
+  if (updateResult.modifiedCount > 0) {
+    return res.json({ result: true, message: 'Mise à jour réussie !' });
+  } else {
+    return res.json({ result: false, message: 'Aucun changement effectuée' });
+  }
+});
 module.exports = router;
