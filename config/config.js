ec217db6f902f8979ed0adad59cc7e40c3218d8e
Sofiane <ahmedzaid.s@gmail.com>
2024-01-15 17:45:32 +0100

    new database

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.CONNECTION_MDB, { connectTimeoutMS: 2000 })
-.then(() => console.log('=> Bien joué mon pote („• ᴗ •„)'))
+.then(() => console.log('=> Bien joué mon pote 😊😊👌👍😍'))
 .catch(error => console.error(error))
